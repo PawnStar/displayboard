2f96a1eac8859dd5aa134843de3fd89a3be5804a
Cole Erickson <pawnstar4@gmail.com>
2016-11-30 17:59:15 -0700

    Added non-root user

--- archlive/airootfs/root/customize_airootfs.sh
+++ archlive/airootfs/root/customize_airootfs.sh
@@ -11,6 +11,10 @@ usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 chmod 700 /root
 
+! id display && useradd -m -s /usr/bin/zsh display
+cp -aT /etc/skel/ /home/display/
+chmod 700 /home/display/
+
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
